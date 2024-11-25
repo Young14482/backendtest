1cf5ebb3cde01403e4323f6d2a1e2a71d929c112
young <young14482@gmail.com>
2024-11-25 16:09:44 +0900

    db생성 및 properties설정

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,5 @@
+insert into user_tb (name) values ('스폰지밥');
+insert into user_tb (name) values ('뚱이');
+insert into user_tb (name) values ('징징이');
+insert into user_tb (name) values ('집게사장');
+insert into user_tb (name) values ('다람이');
